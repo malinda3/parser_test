2e69e8ec09dcad031a0e943b2fa51160db2d3012
malindac <malindac@student.21-school.ru>
2024-09-10 00:36:07 +0300

    finish 0909

--- ProductParser.py
+++ ProductParser.py
@@ -1,58 +1,116 @@
-#ok#https://faworldentertainment.com/collections/bottoms/products/salt-and-pepper-canvas-double-knee-pant
-#ok#https://shop.palaceskateboards.com/products/a7oh8xvpjvqf
-#неправильная цена#https://www.asos.com/asos-design/asos-design-fine-knit-boat-neck-top-in-cream/prd/205945056#colourWayId-205945063
-#ok#https://www.grailed.com/listings/67108758-arc-teryx-x-streetwear-x-vintage-vintage-hat-arcteryx-cap-outdoor-gore-tex-arcteryx?g_aidx=Listing_by_heat_production&g_aqid=02fab20a807c337cc1a14ed9de6d2154
-#не дает ничего выгрузить#https://stockx.com/air-jordan-4-retro-white-thunder?size=4
-#не дает ничего выгрузить#https://www.farfetch.com/nl/shopping/men/palm-angels-hermosa-item-25038217.aspx
-#ok#https://shop.doverstreetmarket.com/collections/comme-des-garcons-play/products/play-unisex-parka-1-carry-over-ax-t344-051-1
-#не дает ничего выгрузить#https://www.ebay.com/itm/126523570030?_nkw=fuckingawesome+t+shirt&itmmeta=01J7AT6Q804YB77VR8C1EGQTXX&hash=item1d7564776e:g:S~cAAOSwFUBmZ4b8&itmprp=enc%3AAQAJAAAA8HoV3kP08IDx%2BKZ9MfhVJKnQqNnglOE7vrjZDWo73ZBuvjZPZ6Ek9rmfm9giPGiBO9D2FlDJzpvQ3OL9UWVt4DEUrR73ycQsFsuc9CfidOpLNAQDn5eTkIDJ%2FEwl8EBbBYchgWkFArjF22Dw%2FybvPqVMMgzM1hyGIVfvQSK3HUeVZhlT9zoRO14iy%2BhRKbugMNTTsEcHyGisNoMgGZGvDEah%2FTWL2ks61ObEkfxjWdsEFSdgOQYni4MevdQx8rdg0XQHSCBsmKRH0acnX9N%2Fv4yjHiNXlQcXx39eSes%2BJaahO8ZrZ0pXJcvgM0D5824cAA%3D%3D%7Ctkp%3ABk9SR4r0mtq6ZA
-#not ok#https://www.carhartt-wip.com/en/men-featured-9/og-detroit-jacket-winter-malbec-black-aged-canvas-964_1
-#ok#https://itkkit.com/catalog/product/246455_thisisneverthat-regular-jeans-red/
-#не дает цену#https://www.drmartens.com/eu/en_eu/sinclair-milled-nappa-leather-platform-boots-black/p/22564001
-#ok#https://fuckthepopulation.com/collections/shop/products/made-in-hell-leather-puffer-coatwhite
-#не дает цену#https://dimemtl.com/collections/dime-fall-24/products/fa24-coverstitch-sherpa-fleece-military-brown
-#ok#https://kith.com/collections/mens-footwear/products/aaih3432
-
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import re
+import logging
 
-ua = UserAgent()
-headers = {'User-Agent': ua.random}
-url = "https://dimemtl.com/collections/dime-fall-24/products/fa24-coverstitch-sherpa-fleece-military-brown"
+logging.basicConfig(level=logging.INFO)
 
-try:
-    response = requests.get(url, headers=headers, timeout=15)
+class ProductParser:
+    def __init__(self, url, timeout=15):
+        self.url = url
+        self.timeout = timeout
+        self.ua = UserAgent()
+        self.headers = {'User-Agent': self.ua.random}
+        self.soup = None
+        self.product_name = None
+        self.product_price = None
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+    def fetch_page(self):
+        try:
+            response = requests.get(self.url, headers=self.headers, timeout=self.timeout)
+            response.raise_for_status()
+            self.soup = BeautifulSoup(response.text, 'html.parser')
+        except requests.exceptions.HTTPError as http_err:
+            logging.error(f"HTTP error occurred: {http_err}")
+        except requests.exceptions.Timeout:
+            logging.error(f"Timeout after {self.timeout} seconds on {self.url}.")
+        except Exception as err:
+            logging.error(f"Other error occurred: {err}")
 
-        product_name_tag = soup.find('h1')
-        if product_name_tag:
-            product_name = product_name_tag.get_text(strip=True)
-        else:
-            product_name = "Name not found"
-            print("Cannot found name in headers.")
-
-        currency_symbols = r'£|\$|€|¥'
-        product_price_tag = soup.find('span', string=lambda text: re.search(currency_symbols, text) if text else False)
-        
-        if product_price_tag:
-            product_price_match = re.search(r'(\£|\$|€|¥)\s?\d+\.?\d*', product_price_tag.get_text(strip=True))
-            if product_price_match:
-                product_price = product_price_match.group(0)
+    def parse_product_name(self):
+        if self.soup:
+            product_name_tag = self.soup.find('h1')
+            if product_name_tag:
+                self.product_name = product_name_tag.get_text(strip=True)
             else:
-                product_price = "Price not found"
-                print("Cannot found price in headers.")
+                self.product_name = "Name not found"
+                logging.warning("Cannot find product name.")
         else:
-            product_price = "Price not found"
-            print("Cannot found price in headers.")
+            logging.error("No content to parse for product name.")
 
-        # Выводим результаты в консоль
-        print(f"Name: {product_name}")
-        print(f"Price: {product_price}")
-    else:
-        print("Cannot GET. ERROR:", response.status_code)
-except requests.exceptions.Timeout:
-    print("Timeout15sec(yoox??).")
+    def parse_product_price(self):
+        if self.soup:
+            currency_symbols = r'£|\$|€|¥'
+            product_price_tag = self.soup.find('span', string=lambda text: re.search(currency_symbols, text) if text else False)
+            
+            if product_price_tag:
+                price_text = product_price_tag.get_text(strip=True)
+                numeric_price = re.search(r'(\d+(\.\d+)?)', price_text)
+                if numeric_price:
+                    self.product_price = int(float(numeric_price.group(0)))
+                else:
+                    self.product_price = "Price not found"
+                    logging.warning("Cannot extract price.")
+            else:
+                self.product_price = "Price not found"
+                logging.warning("Cannot find price element.")
+        else:
+            logging.error("No content to parse for product price.")
+
+
+    def get_product_info(self):
+        self.fetch_page()
+        self.parse_product_name()
+        self.parse_product_price()
+        return {
+            'name': self.product_name,
+            'price': self.product_price
+        }
+        
+    def get_product_name(self):
+        self.fetch_page()
+        self.parse_product_name()
+        return self.product_name
+
+    def get_product_price(self):
+        self.fetch_page()
+        self.parse_product_price()
+        return self.product_price
+
+    @staticmethod
+    def test():
+        urls = [
+            'https://faworldentertainment.com/collections/bottoms/products/salt-and-pepper-canvas-double-knee-pant',
+            'https://shop.palaceskateboards.com/products/a7oh8xvpjvqf',
+            'https://www.asos.com/asos-design/asos-design-fine-knit-boat-neck-top-in-cream/prd/205945056#colourWayId-205945063',
+            'https://www.grailed.com/listings/67108758-arc-teryx-x-streetwear-x-vintage-vintage-hat-arcteryx-cap-outdoor-gore-tex-arcteryx?g_aidx=Listing_by_heat_production&g_aqid=02fab20a807c337cc1a14ed9de6d2154',
+            'https://stockx.com/air-jordan-4-retro-white-thunder?size=4',
+            'https://www.farfetch.com/nl/shopping/men/palm-angels-hermosa-item-25038217.aspx',
+            'https://shop.doverstreetmarket.com/collections/comme-des-garcons-play/products/play-unisex-parka-1-carry-over-ax-t344-051-1',
+            'https://www.ebay.com/itm/126523570030?_nkw=fuckingawesome+t+shirt&itmmeta=01J7AT6Q804YB77VR8C1EGQTXX&hash=item1d7564776e:g:S~cAAOSwFUBmZ4b8&itmprp=enc%3AAQAJAAAA8HoV3kP08IDx%2BKZ9MfhVJKnQqNnglOE7vrjZDWo73ZBuvjZPZ6Ek9rmfm9giPGiBO9D2FlDJzpvQ3OL9UWVt4DEUrR73ycQsFsuc9CfidOpLNAQDn5eTkIDJ%2FEwl8EBbBYchgWkFArjF22Dw%2FybvPqVMMgzM1hyGIVfvQSK3HUeVZhlT9zoRO14iy%2BhRKbugMNTTsEcHyGisNoMgGZGvDEah%2FTWL2ks61ObEkfxjWdsEFSdgOQYni4MevdQx8rdg0XQHSCBsmKRH0acnX9N%2Fv4yjHiNXlQcXx39eSes%2BJaahO8ZrZ0pXJcvgM0D5824cAA%3D%3D%7Ctkp%3ABk9SR4r0mtq6ZA',
+            'https://www.carhartt-wip.com/en/men-featured-9/og-detroit-jacket-winter-malbec-black-aged-canvas-964_1',
+            'https://itkkit.com/catalog/product/246455_thisisneverthat-regular-jeans-red/',
+            'https://www.drmartens.com/eu/en_eu/sinclair-milled-nappa-leather-platform-boots-black/p/22564001',
+            'https://fuckthepopulation.com/collections/shop/products/made-in-hell-leather-puffer-coatwhite',
+            'https://dimemtl.com/collections/dime-fall-24/products/fa24-coverstitch-sherpa-fleece-military-brown',
+            'https://kith.com/collections/mens-footwear/products/aaih3432'
+        ]
+        
+        for url in urls:
+            parser = ProductParser(url)
+            product_info = parser.get_product_info()
+            
+            logging.info(f"URL: {url}")
+            logging.info(f"Name: {product_info['name']}")
+            logging.info(f"Price: {product_info['price']}")
+            logging.info('-' * 40)
+
+#if __name__ == "__main__":
+    #ProductParser.test()
+
+    #url = "https://faworldentertainment.com/collections/bottoms/products/salt-and-pepper-canvas-double-knee-pant"
+    #parser = ProductParser(url)
+    #product_info = parser.get_product_info()
+    #logging.info(f"Name: {product_info['name']}")
+    #logging.info(f"Price: {product_info['price']}")
---
+++ main_bot.py
@@ -0,0 +1,75 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import logging
+from ProductParser import ProductParser 
+import os
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.INFO)
+load_dotenv()
+
+class BotHandler:
+    def __init__(self, token, commission_rate=float(os.getenv('COMMISSION_RATE', 0.1)), additional_fee=float(os.getenv('ADDITIONAL_FEE', 50))):
+        self.token = token
+        self.application = Application.builder().token(self.token).build()
+        self.commission_rate = commission_rate
+        self.additional_fee = additional_fee
+        self.user_data = {}
+
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        user = update.message.from_user
+        logging.info(f"User {user.id} started the bot.")
+        await update.message.reply_text('Send me a product link.')
+
+    async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        user_message = update.message.text
+        user = update.message.from_user
+        logging.info(f"Received message from {user.id}: {user_message}")
+
+        if self.is_valid_url(user_message):
+            logging.info(f"Valid URL received: {user_message}")
+            product_info = self.get_product_info(user_message)
+
+            if product_info['price'] != "Price not found":
+                response = f"Product Name: {product_info['name']}\nProduct Price: {product_info['price']}"
+                await update.message.reply_text(response)
+            else:
+                self.user_data[user.id] = {'name': product_info['name'], 'url': user_message}
+                await update.message.reply_text('Could not find the price. Please enter the price manually:')
+        else:
+            if user.id in self.user_data and self.is_number(user_message):
+                manual_price = float(user_message)
+                final_price = manual_price * (1 + self.commission_rate) + self.additional_fee
+                await update.message.reply_text(
+                    f"Manual Price: {manual_price}\n"
+                    f"Final Price with Commission and Fee: {final_price:.2f}"
+                )
+                del self.user_data[user.id]
+            else:
+                await update.message.reply_text('Please send a valid product link or a valid number.')
+
+    def is_valid_url(self, url: str) -> bool:
+        return url.startswith("http://") or url.startswith("https://")
+
+    def is_number(self, text: str) -> bool:
+        try:
+            float(text)
+            return True
+        except ValueError:
+            return False
+
+    def get_product_info(self, url: str) -> dict:
+        parser = ProductParser(url)
+        return parser.get_product_info()
+
+    def run(self):
+        self.application.add_handler(CommandHandler("start", self.start))
+        self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_message))
+
+        logging.info("Bot is starting...")
+        self.application.run_polling()
+
+if __name__ == "__main__":
+    bot_token = os.getenv('TOKEN')
+    bot = BotHandler(bot_token)
+    bot.run()
