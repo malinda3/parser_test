7869f3f9ab002d07b877ad23170070015c79f58c
malindac <malindac@student.21-school.ru>
2024-09-12 10:00:07 +0300

    bot update

--- bot.py
+++ bot.py
@@ -8,32 +8,20 @@ from dotenv import load_dotenv
 import re
 from datetime import datetime
 
-# Получение текущей даты
 current_date = datetime.now().strftime('%Y-%m-%d')
 
-# Создание папки для логов с текущей датой
 log_directory = f'logs/{current_date}'
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 
-# Настройка логирования
 log_file = f'{log_directory}/bot.log'
 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-
-# Настройка основной конфигурации логирования
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-# Удаление всех существующих обработчиков (если есть)
-for handler in logger.handlers[:]:
-    logger.removeHandler(handler)
-
-# Добавление обработчика для записи в файл
 file_handler = RotatingFileHandler(log_file, maxBytes=10**6, backupCount=5)
 file_handler.setFormatter(logging.Formatter(log_format))
 logger.addHandler(file_handler)
 
-# Добавление обработчика для вывода в консоль
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(logging.Formatter(log_format))
 logger.addHandler(console_handler)
@@ -59,7 +47,6 @@ class BotHandler:
         user = update.message.from_user
         logging.info(f"User {user.id} started the bot.")
 
-        # Создаем кнопки меню
         keyboard = [
             [InlineKeyboardButton("Оформить заказ", callback_data='order')],
             [InlineKeyboardButton("FAQ", url="https://rusale.shop/individual")],
@@ -106,7 +93,6 @@ class BotHandler:
                                     f"Для оформления заказа перешлите это сообщение: https://t.me/rusalemngr")
                         await query.message.edit_text(response)
                         
-                        # Логирование информации о новом заказе
                         logging.info(f"New order created by User {user_id}: "
                                      f"Name: {self.user_data[user_id]['name']}, "
                                      f"Price: {self.user_data[user_id]['price']} {selected_currency}, "
@@ -116,7 +102,7 @@ class BotHandler:
                         del self.user_data[user_id]
                     except ValueError as e:
                         logging.error(f"ValueError: {e}")
-                        await query.message.reply_text(f'Произошла ошибка: {str(e)}')
+                        await query.message.reply_text(f'Произошла ошибка')
                 else:
                     await query.message.reply_text('Что-то пошло не так, попробуйте снова.')
 
@@ -130,7 +116,7 @@ class BotHandler:
         except Exception as e:
             logging.error(f"Error handling menu selection: {e}")
             if query.message:
-                await query.message.reply_text(f'Произошла ошибка: {str(e)}')
+                await query.message.reply_text(f'Произошла ошибка')
 
     async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         user_message = update.message.text
@@ -146,7 +132,6 @@ class BotHandler:
                     self.user_data[user.id] = {'name': product_info['name'], 'price': product_info['price'], 'url': user_message}
                     await self.ask_for_currency(update)
 
-                    # Логирование информации о новом заказе
                     logging.info(f"New order created by User {user.id}: "
                                  f"Name: {product_info['name']}, "
                                  f"Price: {product_info['price']}, "
@@ -167,7 +152,7 @@ class BotHandler:
         except Exception as e:
             logging.error(f"Error handling message: {e}")
             if update.message:
-                await update.message.reply_text(f'Произошла ошибка: {str(e)}')
+                await update.message.reply_text(f'Произошла ошибка')
 
     async def ask_for_currency(self, update: Update) -> None:
         buttons = [InlineKeyboardButton(curr, callback_data=f'currency_{curr.lower()}') for curr in self.currencies.keys()]
