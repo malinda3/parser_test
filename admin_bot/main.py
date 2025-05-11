11d6497031e01424b6a4e77fa0bddd85ebf46bcc
malindac <malindac@tron.arpa>
2025-05-12 00:13:38 +0300

    test

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -1,4 +1,4 @@
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, Bot
 from telegram.ext import (
     Application,
     CommandHandler,
