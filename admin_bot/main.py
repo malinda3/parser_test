32be140eaadc8559d99f62e47c12e1590b227853
malindac <malindac@tron.arpa>
2025-05-11 23:24:30 +0300

    admin

--- admin_bot/main.py
+++ admin_bot/main.py
@@ -86,7 +86,7 @@ async def retranslate_message(update: Update, context: CallbackContext) -> int:
         
         # Если есть фото - показываем его
         if 'media' in context.user_data:
-            await update.message.send_photo(
+            await update.message.reply_photo(
                 photo=context.user_data['media'],
                 caption=preview_text,
                 reply_markup=InlineKeyboardMarkup([
@@ -95,7 +95,7 @@ async def retranslate_message(update: Update, context: CallbackContext) -> int:
                 ])
             )
         else:
-            await update.message.send_text(
+            await update.message.reply_text(
                 preview_text,
                 reply_markup=InlineKeyboardMarkup([
                     [InlineKeyboardButton("✅ Да", callback_data='confirm')],
